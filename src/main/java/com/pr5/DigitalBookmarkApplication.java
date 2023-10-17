6f851f04c6d701c888bb2e1cd5cff6f29cce4a1d
p.korshunov <korshunov.petr@gmail.com>
2023-10-17 12:37:56 +0300

    init

---
+++ src/main/java/com/pr5/DigitalBookmarkApplication.java
@@ -0,0 +1,13 @@
+package com.pr5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalBookmarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalBookmarkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pr5/DigitalBookmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pr5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBookmarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
