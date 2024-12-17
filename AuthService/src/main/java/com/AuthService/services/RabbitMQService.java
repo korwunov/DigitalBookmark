53d79796a961d14f40e891376d9310c1f6bb7945
p.korshunov <korshunov.petr@gmail.com>
2024-12-17 23:33:25 +0300

    add logs

--- AuthService/src/main/java/com/AuthService/services/RabbitMQService.java
+++ AuthService/src/main/java/com/AuthService/services/RabbitMQService.java
@@ -2,6 +2,7 @@ package com.AuthService.services;
 
 import com.BookmarkService.domain.User;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 @Component
 @EnableRabbit
 @RequiredArgsConstructor
+@Log4j
 public class RabbitMQService {
     private final JwtService jwtService;
     private final UserAuthService userAuthService;
@@ -19,14 +21,17 @@ public class RabbitMQService {
         try {
             String tokenWithoutType = new StringBuilder(token).substring(token.indexOf(" ") + 1, token.length());
             String username = jwtService.extractUserName(tokenWithoutType);
-            System.out.println("username from token " + username);
+            log.info("Extracted username from token " + username);
             User user = userAuthService.getByUsername(username);
-            if (user == null) { return null; }
-            System.out.println(user);
+            if (user == null) {
+                log.info("User with username: " + username + " not found");
+                return null;
+            }
+            log.info("Found user with id: " + user);
             return user;
         }
         catch (Exception e) {
-            System.out.println(e.getMessage());
+            log.error("Exception occurred during user login " + e.getMessage());
             return null;
         }
     }
